f2f4439d049874716e6ee7731359b0ef328dcd9c
geka.evk <geka.evk@gmail.com>
2025-09-03 11:22:29 +0100

    fix(csi-1750): fixed seeds not to clean up DB

--- seeds/01_oracle_test_entries.ts
+++ seeds/01_oracle_test_entries.ts
@@ -1,28 +1,11 @@
 'use strict'
 import { Knex } from 'knex'
 import { logger } from '~/shared/logger'
-
-export const msisdns = [
-  {
-    id: '000000',
-    fspId: 'dfspa'
-  },
-  {
-    id: '111111',
-    fspId: 'dfspb'
-  },
-  {
-    id: '222222',
-    fspId: 'dfspc'
-  }
-]
+import msisdns from './01_msisdns.json'
 
 export function seed(knex: Knex): Promise<number[] | void> {
+  logger.info('seed is running...', { msisdns })
   return knex('oracleMSISDN')
     .then(() => knex('oracleMSISDN').insert(msisdns))
-    .then((data) => {
-      logger.info('seed is done: ', { data })
-      return data
-    })
     .catch(err => { logger.error('error in seed: ', err) })
 }
